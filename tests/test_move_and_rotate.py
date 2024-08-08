92d484338b890037c1f9a6d72f4373adb0651c10
alexfilatov99 <alexfilatov99@gmail.com>
2024-08-08 11:16:17 +0300

    исправлены ошибки исключений в тестирующем скрипте

--- tests/test_move_and_rotate.py
+++ tests/test_move_and_rotate.py
@@ -34,17 +34,17 @@ class TestObjectMovement(unittest.TestCase):
         self.assertTrue("Cannot read velocity" in str(context.exception))
 
     def test_move_with_unsettable_position_raises_error(self):
-        obj = Movable_obj(12, 5, -7, 3)
-        obj._x = None  # Simulate error condition for setting position
+        obj = Movable_obj(None, 5, -7, 3)
+        #obj._x = None  # Simulate error condition for setting position
         with self.assertRaises(ValueError) as context:
             obj.move()
-        self.assertTrue("Cannot set position" in str(context.exception))
+        self.assertTrue("Cannot read position" in str(context.exception))
 
-        obj = Movable_obj(12, 5, -7, 3)
-        obj._y = None  # Simulate error condition for setting position
+        obj = Movable_obj(12, None, -7, 3)
+        #obj._y = None  # Simulate error condition for setting position
         with self.assertRaises(ValueError) as context:
             obj.move()
-        self.assertTrue("Cannot set position" in str(context.exception))
+        self.assertTrue("Cannot read position" in str(context.exception))
 
 class TestObjectRotate(unittest.TestCase):
     def test_rotate_changes_angle(self):
@@ -61,8 +61,8 @@ class TestObjectRotate(unittest.TestCase):
     def test_rotate_with_unreadable_angVelocity_raises_error(self):
         obj = Rotatable_obj(5, None)
         with self.assertRaises(ValueError) as context:
-            obj.rotate()
-        self.assertTrue("Cannot read angVelocity" in str(context.exception))
+            obj.rotate()  
+        self.assertTrue("Cannot read angle or angular velocity" in str(context.exception))
 
 if __name__ == '__main__':
     unittest.main()
